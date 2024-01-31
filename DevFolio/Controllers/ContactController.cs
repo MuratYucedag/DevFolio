fa80d43f30dce2a423df7754b3c3b86d0cf41e10
MuratYucedag <yucedagmurat23@gmail.com>
2024-01-31 20:46:52 +0300

    Add project files.

---
+++ DevFolio/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+        public ActionResult MessageList()
+        {
+            return View();
+        }
+    }
+}
+//camelCase
+//PascalCase
+//snakecase
+//Değişkenler-Metotlar-Field-Property-Sınıf-Interface
